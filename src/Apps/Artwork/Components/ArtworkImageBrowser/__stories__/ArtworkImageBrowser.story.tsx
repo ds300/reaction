b69934967fc2c576eb26438e827af3c576ecccb2
David Sheldrick <d.j.sheldrick@gmail.com>
2019-06-13 12:28:07 +0100

    add stories for geometric comparison

--- src/Apps/Artwork/Components/ArtworkImageBrowser/__stories__/ArtworkImageBrowser.story.tsx
+++ src/Apps/Artwork/Components/ArtworkImageBrowser/__stories__/ArtworkImageBrowser.story.tsx
@@ -1,12 +1,20 @@
-import { Box, Col } from "@artsy/palette"
+import {
+  Box,
+  Col,
+  Flex,
+  Join,
+  Sans,
+  Separator,
+  Serif,
+  Spacer,
+} from "@artsy/palette"
 import { ArtworkImageBrowserQueryRenderer } from "Apps/Artwork/Components/ArtworkImageBrowser"
 import React from "react"
 import { storiesOf } from "storybook/storiesOf"
 import { Section } from "Utils/Section"
 
-storiesOf("Apps/Artwork Page/Components/ArtworkImageBrowser", module).add(
-  "ArtworkBrowser",
-  () => {
+storiesOf("Apps/Artwork Page/Components/ArtworkImageBrowser", module)
+  .add("ArtworkBrowser", () => {
     return (
       <>
         <Section title="Multiple images (responsive)">
@@ -21,5 +29,91 @@ storiesOf("Apps/Artwork Page/Components/ArtworkImageBrowser", module).add(
         </Section>
       </>
     )
-  }
-)
+  })
+
+  .add("Fonts!", () => {
+    return (
+      <Flex ml={40} flexDirection="column">
+        <Flex pb="2" background="palevioletred" />
+        <Join separator={<Separator />}>
+          <Sans size="0" bg="papayawhip">
+            This is some
+            <br /> text 0
+          </Sans>
+          <Sans size="1" bg="papayawhip">
+            This is some
+            <br />
+            text 1
+          </Sans>
+          <Sans size="2" bg="papayawhip">
+            This is some
+            <br />
+            text 2
+          </Sans>
+          <Sans size="3" bg="papayawhip">
+            This is some
+            <br />
+            text 3
+          </Sans>
+          <Sans size="3t" bg="papayawhip">
+            This is some
+            <br />
+            text 3t
+          </Sans>
+          <Sans size="4" bg="papayawhip">
+            This is some
+            <br />
+            text 4
+          </Sans>
+          <Sans size="4t" bg="papayawhip">
+            This is some
+            <br />
+            text 4t
+          </Sans>
+        </Join>
+        <Spacer mb={2} />
+        <Join separator={<Separator />}>
+          <Serif size="1" bg="papayawhip">
+            This is some
+            <br />
+            text 1
+          </Serif>
+          <Serif size="2" bg="papayawhip">
+            This is some
+            <br />
+            text 2
+          </Serif>
+          <Serif size="3" bg="papayawhip">
+            This is some
+            <br />
+            text 3
+          </Serif>
+          <Serif size="3t" bg="papayawhip">
+            This is some
+            <br />
+            text 3t
+          </Serif>
+          <Serif size="4" bg="papayawhip">
+            This is some
+            <br />
+            text 4
+          </Serif>
+          <Serif size="4t" bg="papayawhip">
+            This is some
+            <br />
+            text 4t
+          </Serif>
+          <Serif size="5" bg="papayawhip">
+            This is some
+            <br />
+            text 5t
+          </Serif>
+          <Serif size="5t" bg="papayawhip">
+            This is some
+            <br />
+            text 5t
+          </Serif>
+        </Join>
+      </Flex>
+    )
+  })
